21e0a167764ab04ca69d8143b2cff5815d3b7b04
pyromaniac3 <97550706+pyromaniac3@users.noreply.github.com>
2023-05-02 13:52:32 -0700

    updated time to be a minute

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -103,14 +103,14 @@ class Play extends Phaser.Scene{
 
         // 60-second play clock
         scoreConfig.fixedWidth = 0;
-        this.clock = this.time.delayedCall(10000, () => {
+        this.clock = this.time.delayedCall(60000, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
             this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
             this.gameOver = true;
         }, null, this);
 
         // creating a dealyed call to move the ships fast after 30 seconds
-        this.timer = this.time.delayedCall(5000, () => {
+        this.timer = this.time.delayedCall(30000, () => {
             this.ship01.moveSpeed = 6;     
             this.ship02.moveSpeed = 6;
             this.ship03.moveSpeed = 6;
